7a0f4923070dd3adc4690c7f2b6d84a8ef9c7661
t4ulquiorra <t4.ulquiorra@email.com>
2026-05-05 12:06:26 +0530

    Fix duplicate protobuf plugin entries

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,39 +4,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.aboutlibraries) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.aboutlibraries.multiplatform) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.room) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.sentry.gradle) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.android.lint) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.build.config) apply false
-    alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.osdetector) apply false
 }
 
