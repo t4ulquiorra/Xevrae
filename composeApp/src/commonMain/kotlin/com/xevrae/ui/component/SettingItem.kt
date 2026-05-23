f317efe8234a97ee3b7e8a0c16e12595c27628f2
Dare Dev <dev@dare.music>
2026-05-23 14:13:32 +0530

    Fix: add remember and MutableInteractionSource to SettingItem

--- composeApp/src/commonMain/kotlin/com/xevrae/ui/component/SettingItem.kt
+++ composeApp/src/commonMain/kotlin/com/xevrae/ui/component/SettingItem.kt
@@ -1,6 +1,8 @@
 package com.xevrae.ui.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
