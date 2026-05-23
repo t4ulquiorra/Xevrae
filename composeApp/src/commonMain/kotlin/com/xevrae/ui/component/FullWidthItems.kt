e5b81fabb8eb6614f31280363a93785bdf3f81f0
Dare Dev <dev@dare.music>
2026-05-23 14:03:24 +0530

    Fix: add MutableInteractionSource import to FullWidthItems and SettingItem

--- composeApp/src/commonMain/kotlin/com/xevrae/ui/component/FullWidthItems.kt
+++ composeApp/src/commonMain/kotlin/com/xevrae/ui/component/FullWidthItems.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
