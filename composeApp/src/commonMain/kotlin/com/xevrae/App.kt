bae9fd977c732df16999472a91839922d2042552
Dare Dev <dev@dare.music>
2026-06-02 06:15:15 +0530

    Add 8dp gap back between home and now playing panel

--- composeApp/src/commonMain/kotlin/com/xevrae/App.kt
+++ composeApp/src/commonMain/kotlin/com/xevrae/App.kt
@@ -461,7 +461,7 @@ fun App(viewModel: SharedViewModel = koinInject()) {
                                             .fillMaxHeight()
                                             .fillMaxWidth(0.35f),
                                     ) {
-                                        Spacer(Modifier.width(15.dp))
+                                        Spacer(Modifier.width(8.dp))
                                         Box(
                                             Modifier
                                                 .padding(
