682a6aa229aa2ed5b02e192c13da8d8e0b6df557
Dare Dev <dev@dare.music>
2026-06-03 10:17:28 +0530

    fix: add missing fillMaxSize import in HomeScreen

--- composeApp/src/commonMain/kotlin/com/xevrae/ui/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/xevrae/ui/screen/home/HomeScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.exclude
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
