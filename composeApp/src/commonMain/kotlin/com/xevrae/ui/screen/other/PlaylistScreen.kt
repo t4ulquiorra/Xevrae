3ffb816181c54f8e28c8b15ebe066b52327e0843
t4ulquiorra <t4.ulquiorra@gmail.com>
2026-08-24 17:10:13 +0530

    Add missing BoxWithConstraints import in PlaylistScreen.kt

--- composeApp/src/commonMain/kotlin/com/xevrae/ui/screen/other/PlaylistScreen.kt
+++ composeApp/src/commonMain/kotlin/com/xevrae/ui/screen/other/PlaylistScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
